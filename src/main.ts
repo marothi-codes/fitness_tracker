c710b99fb01c5740b7eb88ce5ecd0dd47bbdb45d
Marothi Mahlake <marothi.codes@gmail.com>
2024-06-15 07:51:59 +0200

    hammerjs added for gesture support

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import 'hammerjs';
+
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
