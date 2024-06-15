a472174797fec1d1e4da008461867b5ade5cbb14
Marothi Mahlake <marothi.codes@gmail.com>
2024-06-15 07:47:04 +0200

    material module defined to outsource material module imports and exports in a centralized manner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    MaterialModule
   ],
   providers: [
     provideAnimationsAsync()
