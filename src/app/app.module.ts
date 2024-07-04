4515c97dcd6b8c8afc6f1ddb637ac8595c281f24
Marothi Mahlake <marothi.codes@gmail.com>
2024-07-04 07:26:06 +0200

    flex layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
 import { SignUpComponent } from './pages/auth/sign-up/sign-up.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,8 @@ import { WelcomeComponent } from './pages/welcome/welcome.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    FlexLayoutModule,
+    FormsModule,
     MaterialModule
   ],
   providers: [
