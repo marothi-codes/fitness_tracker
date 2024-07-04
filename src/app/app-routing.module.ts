de6e876eead4733eabb6bd557ca41572e64747b1
Marothi Mahlake <marothi.codes@gmail.com>
2024-07-04 07:26:37 +0200

    routes ordering

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { SignUpComponent } from './pages/auth/sign-up/sign-up.component';
 import { LoginComponent } from './pages/auth/login/login.component';
-import { TrainingRoutingModule } from './pages/training/training-routing.module';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'login', component: LoginComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'training', loadChildren: () => import('./pages/training/training.module').then(m => m.TrainingModule) },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
-    TrainingRoutingModule
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
