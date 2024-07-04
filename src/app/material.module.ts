f8a5c91a972f821a340ca35ceb12a05bbc562f4a
Marothi Mahlake <marothi.codes@gmail.com>
2024-07-04 07:26:59 +0200

    form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import { MatFormField, MatInputModule } from "@angular/material/input";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
 
 @NgModule({
     imports: [
         MatButtonModule,
+        MatDatepickerModule,
+        MatMomentDateModule,
+        MatFormField,
+        MatInputModule,
         MatIconModule
     ],
     exports: [
         MatButtonModule,
-        MatIconModule
+        MatDatepickerModule,
+        MatMomentDateModule,
+        MatFormField,
+        MatIconModule,
+        MatInputModule
     ]
 })
 export class MaterialModule {
--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm, ValidationErrors } from '@angular/forms';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { CUSTOM_DATE_FORMATS } from '../../../constants/custom-date-formats';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrls: ['./sign-up.component.css'],
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS },
+    { provide: MAT_DATE_LOCALE, useValue: 'en-ZA'}
+  ]
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+    } else {
+      Object.keys(form.controls).forEach(key => {
+        const controlErrors: ValidationErrors = form.controls[key].errors;
+        if (controlErrors != null) {
+          Object.keys(controlErrors).forEach(keyError => {
+            console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+          });
+        }
+      });
+    }
+  }
 
 }
