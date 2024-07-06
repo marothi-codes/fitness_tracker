bae155f7cb4d70b7de4a5528098efa7bc3ab5d41
Marothi Mahlake <marothi.codes@gmail.com>
2024-07-06 14:27:37 +0200

    login form

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+    } else {
+      Object.keys(form.controls).forEach(key => {
+        const controlErrors: ValidationErrors = form.controls[key].errors;
+        if (controlErrors != null) {
+          Object.keys(controlErrors).forEach(keyError => {
+            console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+          });
+        }
+      });
+    }
+  }
 }
